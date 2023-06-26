40ef892b0e0257b1bd4c7231b65047c231f7c474
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-26 19:10:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ function volume_sphere() {
 	let volume = (4/3)*Math.PI*Math.pow(input,3);
 	
 	document.getElementsByTagName("input")[1].value = volume;
-// var radius = document.getElementById("radius").value;
-
-//   // Calculate the volume using the formula
-//   var volume = (4/3) * Math.PI * Math.pow(radius, 3);
-
-//   // Display the volume in the output field
-//   document.getElementById("volume").value = volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
