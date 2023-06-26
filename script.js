9216d2f92ddbcf3825bc892383ae439eb13fcbbe
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-26 19:49:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function volume_sphere() {
     //Write your code here
-	let input = parseFloat(document.getElementById("radius").value);
-  const volume = (4 / 3) * Math.PI * Math.pow(input, 3);
+	let input = document.getElementById("radius").value;
+  const volume = (4 / 3) * (Math.PI) * Math.pow(input, 3);
+
+	// document.getElementById("res").innerText = volume;
+	// p.innerText(volume);
 	
-  document.getElementById("volume").value = volume.toFixed(2);
+  document.getElementById("volume").value = volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
